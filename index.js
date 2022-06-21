1477a64c42ddff69306cdbd206164260f2b1ba0a
CrazyOutOff <dadouneprank@gmail.com>
2022-06-21 21:05:23 +0200

    Patch invalid api key

--- index.js
+++ index.js
@@ -12,24 +12,28 @@ class Rinaorc {
 
     async player(name){
         let fetched = await this.instance.get(`/player/${name}`).catch(err => {return err})
+        if(fetched?.response?.status === 401 && this.debug) return new Error("Invalid API KEY")
         if(fetched?.response?.status === 404 && this.debug) return new Error("Player Not Found")
         else return fetched.data
     }
 
     async clan(name){
         let fetched = await this.instance.get(`/clan/${name}`).catch(err => {return err})
+        if(fetched?.response?.status === 401 && this.debug) return new Error("Invalid API KEY")
         if(fetched?.response?.status === 404 && this.debug) return new Error("Clan Not Found")
         else return fetched.data
     }
 
     async staff(){
         let fetched = await this.instance.get(`/staff`).catch(err => {return err})
+        if(fetched?.response?.status === 401 && this.debug) return new Error("Invalid API KEY")
         if(fetched?.response?.status === 404 && this.debug) return new Error("An error occured")
         else return fetched.data
     }
 
     async playersStats(){
         let fetched = await this.instance.get(`/stats/players`).catch(err => {return err})
+        if(fetched?.response?.status === 401 && this.debug) return new Error("Invalid API KEY")
         if(fetched?.response?.status === 404 && this.debug) return new Error("An error occured")
         else return fetched.data
     }
